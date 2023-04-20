e84e2eb0b6b350fed818449f6dda91c605943d9b
zypherscript <kitti.kthp@gmail.com>
2023-04-20 21:38:35 +0700

    up

--- src/main/java/com/example/demo/controller/GlobalController.java
+++ src/main/java/com/example/demo/controller/GlobalController.java
@@ -11,11 +11,12 @@ import org.springframework.http.MediaType;
 import org.springframework.messaging.rsocket.RSocketRequester;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
 import reactor.core.publisher.Flux;
 
-@org.springframework.web.bind.annotation.RestController
+@RestController
 @RequiredArgsConstructor
-public class RestController {
+public class GlobalController {
 
   private final CustomerRepository customerRepository;
   private final IntervalMessageProducer intervalMessageProducer;
