c55d015366d8c9796af576418a1db3ca89d6b335
zypherscript <kitti.kthp@gmail.com>
2023-05-12 03:06:43 +0700

    test: add CustomerRepositoryTest

---
+++ src/test/java/com/example/demo/repository/CustomerRepositoryTest.java
@@ -0,0 +1,59 @@
+package com.example.demo.repository;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+import com.example.demo.model.Customer;
+import java.util.Arrays;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.data.r2dbc.DataR2dbcTest;
+import org.springframework.r2dbc.core.DatabaseClient;
+import org.springframework.test.context.ActiveProfiles;
+import reactor.core.publisher.Flux;
+import reactor.test.StepVerifier;
+
+@DataR2dbcTest
+@ActiveProfiles("test")
+class CustomerRepositoryTest {
+
+  @Autowired
+  private DatabaseClient databaseClient;
+  @Autowired
+  private CustomerRepository customerRepository;
+
+  @Test
+  public void testDatabaseClientExisted() {
+    assertNotNull(databaseClient);
+  }
+
+  @Test
+  public void testPostRepositoryExisted() {
+    assertNotNull(customerRepository);
+  }
+
+  @Test
+  public void testInsertAndQuery() {
+    var statements = Arrays.asList(
+        "drop table if exists customer;",
+        "create table customer(id serial primary key, name varchar(255) not null);");
+
+    statements.forEach(sql -> databaseClient.sql(sql)
+        .fetch()
+        .rowsUpdated()
+        .as(StepVerifier::create)
+        .expectNextCount(1)
+        .verifyComplete());
+
+    Flux.just("test1", "test2")
+        .map(n -> new Customer(null, n))
+        .flatMap(customerRepository::save)
+        .subscribe();
+
+    customerRepository.findAll()
+        .as(StepVerifier::create)
+        .consumeNextWith(customer -> assertEquals("test1", customer.getName()))
+        .consumeNextWith(customer -> assertEquals("test2", customer.getName()))
+        .verifyComplete();
+  }
+}
