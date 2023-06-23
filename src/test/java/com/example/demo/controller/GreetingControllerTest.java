93beb795d721fb743f17e68acc14ad14d2e517f0
zypherscript <kitti.kthp@gmail.com>
2023-06-23 12:40:35 +0700

    mc with thymeleaf

---
+++ src/test/java/com/example/demo/controller/GreetingControllerTest.java
@@ -0,0 +1,39 @@
+package com.example.demo.controller;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.reactive.WebFluxTest;
+import org.springframework.test.web.reactive.server.WebTestClient;
+
+@WebFluxTest(GreetingController.class)
+class GreetingControllerTest {
+
+  @Autowired
+  private WebTestClient webTestClient;
+
+  @Test
+  public void testGreetingWithDefaultName() {
+    webTestClient.get().uri("/greeting-view")
+        .exchange()
+        .expectStatus().isOk()
+        .expectBody(String.class)
+        .value(body -> {
+          assertThat(body).contains("Hello, World!");
+        });
+  }
+
+  @Test
+  public void testGreetingWithNameParameter() {
+    String name = "test";
+
+    webTestClient.get().uri("/greeting-view?name={name}", name)
+        .exchange()
+        .expectStatus().isOk()
+        .expectBody(String.class)
+        .value(body -> {
+          assertThat(body).contains("Hello, " + name + "!");
+        });
+  }
+}
